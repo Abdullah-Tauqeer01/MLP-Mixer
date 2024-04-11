0426728acdf37355804d299594afdbbe4c3d6204
Abdullah-Tauqeer01 <160441817+Abdullah-Tauqeer01@users.noreply.github.com>
2024-04-11 15:50:49 -0400

    Add files via upload

--- models/load_weight.py
+++ models/load_weight.py
@@ -9,7 +9,7 @@ from torch import nn
 a = ["token_mixing/Dense_0","token_mixing/Dense_1","channel_mixing/Dense_0","channel_mixing/Dense_1"]
 
 def load_weights(model, weights):
-    weights = np.load(weights)
+    weights = np.load(weights, allow_pickle=True)
     with torch.no_grad():
         if model.zero_head:  ### remove this if add it in training loop
             nn.init.zeros_(model.head.weight)
@@ -37,10 +37,10 @@ def load_weights(model, weights):
                     k = i
                     if k == 2:
                         k = 1
-                    block.token_mlp[i].weight.copy_(torch.from_numpy(weights[pjoin(ROOT, a[k], "kernel")]).t())
-                    block.token_mlp[i].bias.copy_(torch.from_numpy(weights[pjoin(ROOT, a[k], "bias")]).t())
-                    block.channel_mlp[i+1].weight.copy_(torch.from_numpy(weights[pjoin(ROOT, a[k + 2], "kernel")]).t())
-                    block.channel_mlp[i+1].bias.copy_(torch.from_numpy(weights[pjoin(ROOT, a[k + 2], "bias")]).t())
+                    block.token_mlp[i].weight.copy_(torch.from_numpy(weights[ROOT+"/"+ a[k]+"/"+ "kernel"]).t())
+                    block.token_mlp[i].bias.copy_(torch.from_numpy(weights[ROOT+"/"+ a[k]+"/"+ "bias"]).t())
+                    block.channel_mlp[i+1].weight.copy_(torch.from_numpy(weights[ROOT+"/"+ a[k + 2]+"/"+ "kernel"]).t())
+                    block.channel_mlp[i+1].bias.copy_(torch.from_numpy(weights[ROOT+"/"+ a[k + 2]+"/"+ "bias"]).t())
 
                 # print(f'with intialization')
                 # for k,i in enumerate(block.token_mlp_block.layers):
@@ -48,7 +48,7 @@ def load_weights(model, weights):
                 #         print(type(i))
                 #         print(i.weight)
 
-                block.pre_norm.weight.copy_(torch.from_numpy(weights[pjoin(ROOT, "LayerNorm_0", "scale")]))
-                block.pre_norm.bias.copy_(torch.from_numpy(weights[pjoin(ROOT, "LayerNorm_0", "bias")]))
-                block.post_norm.weight.copy_(torch.from_numpy(weights[pjoin(ROOT, "LayerNorm_1", "scale")]))
-                block.post_norm.bias.copy_(torch.from_numpy(weights[pjoin(ROOT, "LayerNorm_1", "bias")]))   
+                block.pre_norm.weight.copy_(torch.from_numpy(weights[ROOT+"/"+ "LayerNorm_0"+"/"+ "scale"]))
+                block.pre_norm.bias.copy_(torch.from_numpy(weights[ROOT+"/"+ "LayerNorm_0"+"/"+ "bias"]))
+                block.post_norm.weight.copy_(torch.from_numpy(weights[ROOT+"/"+ "LayerNorm_1"+"/"+ "scale"]))
+                block.post_norm.bias.copy_(torch.from_numpy(weights[ROOT+"/"+ "LayerNorm_1"+"/"+ "bias"]))   
